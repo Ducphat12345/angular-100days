27894ded79e3aeb12c1e9b4a546ccadb79459568
Ducphat12345 <phatngduc@gmail.com>
2023-09-09 13:00:30 +0700

    ngOnInit-ngOnChanges-Constructor-angular

--- src/progress-bar/progress-bar.component.ts
+++ src/progress-bar/progress-bar.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 
 @Component({
     selector: 'progress-bar',
     templateUrl: './progress-bar.component.html',
     styleUrls: ['./progress-bar.component.css']
 })
-export class ProgressBarComponent{
+export class ProgressBarComponent implements OnInit, OnChanges{
     @Input() progress = 50;
     @Input() backgroundColor = '#ccc';
     @Input() progressColor = 'tomato';
+
+    constructor(){
+        console.log({
+            progress: this.progress,
+            backgroundColor: this.backgroundColor,
+            progressColor: this.progressColor
+        });
+    }
+
+    ngOnInit(): void {
+        console.log('init', {
+            progress: this.progress,
+            backgroundColor: this.backgroundColor,
+            progressColor: this.progressColor
+        });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('changes', {
+            progress: this.progress,
+            backgroundColor: this.backgroundColor,
+            progressColor: this.progressColor
+        });
+    }
 }
